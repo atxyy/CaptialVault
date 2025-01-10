3a0c746a4cbf7b1dca490ed6ad86ccacd51ba5a2
atxyy <147442313+atxyy@users.noreply.github.com>
2025-01-10 02:11:41 +0000

    fixed background?

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        gradient:
-          "linear-gradient(60deg, #f79533, #f37055, #ef4e7b, #a166ab, #5073b8, #1098ad, #07b39b, #6fba82)",
+        gradient: "",
       },
       animation: {
         opacity: "opacity 0.25s ease-in-out",
